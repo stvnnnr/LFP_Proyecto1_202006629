c5dbaf6bf1996e8e52313e79f354b0271a6d7d64
wilber <3605181090101@ingenieria.usac.edu.gt>
2021-09-23 21:16:09 -0600

    Espero sea el final

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import sys
 from tkinter import filedialog, Tk
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtGui import QIcon, QImage, QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from AnaLexico import *
 import webbrowser
 import imgkit
@@ -18,6 +20,12 @@ class principal(QMainWindow):
         self.botonCargar.clicked.connect(self.cargaArchivo)
         self.botonAnalizar.clicked.connect(self.analizar)
         self.botonReporte.clicked.connect(self.abrirReportes)
+        self.botonOriginal.clicked.connect(self.meterImg)
+        self.botonSalir.clicked.connect(self.salirr)
+        self.botonX.clicked.connect(self.meterImgX)
+        self.botonY.clicked.connect(self.meterImgY)
+        self.botonDouble.clicked.connect(self.meterImgD)
+        
 
     def cargaArchivo(self):
         global archivo
@@ -61,6 +69,28 @@ class principal(QMainWindow):
         webbrowser.open("Reporte Errores.html")
         #webbrowser.open("imagenHtml.html")
 
+    def meterImg(self):
+        pixmap = QPixmap('Imagen.jpg')
+        self.frameImagen.setPixmap(pixmap)
+        self.frameImagen.resize(pixmap.width(),pixmap.height())
+
+    def meterImgX(self):
+        pixmap = QPixmap('mirrorX.png')
+        self.frameImagen.setPixmap(pixmap)
+        self.frameImagen.resize(pixmap.width(),pixmap.height())
+
+    def meterImgY(self):
+        pixmap = QPixmap('mirrorY.png')
+        self.frameImagen.setPixmap(pixmap)
+        self.frameImagen.resize(pixmap.width(),pixmap.height())
+
+    def meterImgD(self):
+        pixmap = QPixmap('doubleMirror.png')
+        self.frameImagen.setPixmap(pixmap)
+        self.frameImagen.resize(pixmap.width(),pixmap.height())
+
+    def salirr(self):
+        sys.exit()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
